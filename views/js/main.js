891e13615c008ee29d33c99d21dbeaf2556e7136
dawg790 <nick@wispcreekdesign.com>
2015-03-17 15:42:25 -0600

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -469,7 +469,7 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 50; i++) {
+for (var i = 2; i < 25; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
@@ -502,7 +502,8 @@ var items = document.getElementsByClassName('mover');
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  // var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  var phase = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
     // items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     var prop = "translateX(" + phase + "px)";
