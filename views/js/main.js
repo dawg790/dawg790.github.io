06d8c8bb7c0b9125af5408068f6540305c5956c1
dawg790 <nick@wispcreekdesign.com>
2015-03-17 15:28:43 -0600

    Reset phase variable calculation

--- views/js/main.js
+++ views/js/main.js
@@ -504,7 +504,7 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   for (var i = 0; i < items.length; i++) {
-    var phase = document.body.scrollTop / 1250;
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
