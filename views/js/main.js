0130a87a3abca9d0a42244df48f38995aeb2b0a0
dawg790 <nick@wispcreekdesign.com>
2015-03-17 15:35:29 -0600

    Trying CSS3 transform property again

--- views/js/main.js
+++ views/js/main.js
@@ -502,10 +502,11 @@ var items = document.getElementsByClassName('mover');
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-
+  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    // items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var prop = "translateX(" + phase + "px)";
+    items[i].css("transform", prop);
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
